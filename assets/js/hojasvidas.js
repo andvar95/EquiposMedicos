fc2a7341ccc1ebc5a0f73702cd711219350f5ec6
cristhian <disruptivecrisfon6@gmail.com>
2021-05-19 15:27:27 -0500

    work

---
+++ assets/js/hojasvidas.js
@@ -0,0 +1,16 @@
+const mantains = document.getElementById('listMantains');
+const pagination = document.getElementById('pagination');
+const searchBtn = document.getElementById('searchBtn');
+const getInfoBtn = document.getElementById('getInfo');
+
+// draw row for each mantain order by date
+const drawMantains = () => {
+
+    }
+    // connection api for get data
+const getMantains = () => {
+
+}
+const drawPagination = () => {
+
+}
