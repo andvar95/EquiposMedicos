2e9e8fc7778788d64e15c7c00a104146dd5280de
andvar95 <varleo.95@gmail.com>
2021-05-19 22:49:33 -0500

    a

---
+++ assets/js/hojavida.js
@@ -0,0 +1,39 @@
+// const path =
+//     import ('path');
+
+// class Employ {
+//     constructor(name, position, salary) {
+//         this.name = name;
+//         this.position = position;
+//         this.salary = salary;
+//     }
+// }
+// const loadDB = async() => {
+//         console.log(await __dirname);
+//         // console.log(path.join(__dirname, '../../db/db.json'));
+//         const { users } = await fetch(path.join(__dirname, '../../db/db.json'));
+//         console.log(users);
+//         return users;
+
+//     }
+//     // loadDB().then(res => {
+//     //     console.log(res);
+//     // });
+// console.log(loadDB());
+
+const mantains = document.getElementById('listMantains');
+const pagination = document.getElementById('pagination');
+const searchBtn = document.getElementById('searchBtn');
+const getInfoBtn = document.getElementById('getInfo');
+
+// draw row for each mantain order by date
+const drawMantains = () => {
+
+    }
+    // connection api for get data
+const getMantains = () => {
+
+}
+const drawPagination = () => {
+
+}
